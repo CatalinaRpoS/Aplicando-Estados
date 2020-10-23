f048464d1048b3867550c7decd89967b39454583
CatalinaRpoS <rposalgadoc@gmail.com>
2020-10-22 23:58:20 -0500

    Correcciones títulos

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -7,7 +7,7 @@ class Navbar extends React.Component{
             <div>
                 <nav className="navbar navbar-expand-lg navbar-light">
                     <a className="navbar-brand" href="/"><img src="https://img.icons8.com/bubbles/70/000000/filled-star.png"/>
-                    <span className="font-weight-bold">Aprendamos Astronomía</span>
+                    <span id="titulo" className="font-weight-bold">Aprendamos Astronomía</span>
                     </a>
                 </nav> 
             </div>
